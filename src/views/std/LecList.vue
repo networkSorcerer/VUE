b9a8cc2f29e6daa2e75520df919fce3ae4ec029e
networkSorcerer <gigattfyhig@gmail.com>
2024-06-12 10:36:32 +0900

    vue

--- src/views/std/LecList.vue
+++ src/views/std/LecList.vue
@@ -1,105 +1,120 @@
 <template>
-    <div class="container mt-4">
+  <div class="container mt-4">
+    <div>
       <div>
         <div>
-          <div>
-            <div>
-              <p>/학습지원 / 강의 목록</p>
-            </div>
-            <div>
-              <h3>강의 목록</h3>
-            </div>
-            <div class="mb-3">
-              <select class="form-select w-auto d-inline-block">
-                <option>전체</option>
-                <option>강사명</option>
-                <option>강의명</option>
-              </select>
-              <input type="text" class="form-control w-auto d-inline-block mx-2">
-              <button class="btn btn-primary">검색</button>
-            </div>
-            <div>
-              <table class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th>강의명</th>
-                    <th>수강대상</th>
-                    <th>강사명</th>
-                    <th>개강일</th>
-                    <th>종강일</th>
-                    <th>수강인원</th>
-                    <th>최대인원</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="data in dataList" :key="data.loginID">
-                    <td @click="modalHandler(data.loginID)" class="cursor-pointer">{{ data.lec_name }}</td>
-                    <td>{{ data.sort }}</td>
-                    <td>{{ data.name }}</td>
-                    <td>{{ data.start_date }}</td>
-                    <td>{{ data.end_date }}</td>
-                    <td>{{ data.max_pnum }}</td>
-                    <td>{{ data.pre_pnum }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+          <p>/학습지원 / 강의 목록</p>
+        </div>
+        <div>
+          <h3>강의 목록</h3>
+        </div>
+        <div class="mb-3">
+          <select class="form-select w-auto d-inline-block">
+            <option>전체</option>
+            <option>강사명</option>
+            <option>강의명</option>
+          </select>
+          <input type="text" class="form-control w-auto d-inline-block mx-2">
+          <button class="btn btn-primary">검색</button>
+        </div>
+        <div>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>강의명</th>
+                <th>수강대상</th>
+                <th>강사명</th>
+                <th>개강일</th>
+                <th>종강일</th>
+                <th>수강인원</th>
+                <th>최대인원</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="data in dataList" :key="data.loginID">
+                <td @click="modalHandler(data)" class="cursor-pointer">{{ data.lec_name }}</td>
+                <td>{{ data.sort }}</td>
+                <td>{{ data.name }}</td>
+                <td>{{ data.start_date }}</td>
+                <td>{{ data.end_date }}</td>
+                <td>{{ data.max_pnum }}</td>
+                <td>{{ data.pre_pnum }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-      <RegiLec v-if="modalBoolean" @closeModal="modalBoolean = $event" :loginID="loginID" :lec_id="lec_id" @closeAndSearch="modalClose" />
-      <Pagination v-bind="{currentPage, totalItems: Total, itemsPerPage: 5}" @search="searchLecture($event)" v-if="dataList.length > 0" />
     </div>
-  </template>
-  
-  <script setup>
-  import RegiLec from './RegiLec.vue';
-  import { ref, onMounted } from 'vue';
-  import axios from 'axios';
-  
-  const dataList = ref([]);
-  const currentPage = ref(0);
-  const loginID = ref(0);
-  const modalBoolean = ref(false);
-//   const startpos = ref(0);
-  
-  const modalClose = (param) => {
-    modalBoolean.value = param;
-    LecList();
-  };
-  
-  const modalHandler = (param) => {
-    modalBoolean.value = true;
-    loginID.value = param;
-  };
-  
-  const LecList = (cpage = 1) => {
+    <RegiLec v-if="modalBoolean" 
+      @closeModal="modalBoolean = $event" 
+      :loginID="loginID" 
+      :lec_id="lec_id" 
+      :lecInfo="lecInfo"
+      :weekplan="weekplan"
+      @closeAndSearch="modalClose" />
+    <Pagination v-bind="{currentPage, totalItems: Total, itemsPerPage: 5}" @search="searchLecture($event)" v-if="dataList.length > 0" />
+  </div>
+</template>
 
-    let param = new URLSearchParams();
-    // param.append('std_id', loginID);
-    param.append('currentPage', cpage);
-    param.append('pageSize', 5);
-    param.append('searchInfo', '');
-    param.append('searchKey', '');
-    // param.append('startpos', startpos.value);
-    
-    axios.post('/std/lecListJson.do', param).then((res) => {
-        console.log('res 입니다',res);
-      dataList.value = res.data.lecList;
-      currentPage.value = cpage;
-    }).catch((error) => {
-      console.error('Error fetching lecture list:', error);
-    });
-  };
-  
-  onMounted(() => {
-    LecList();
+<script setup>
+import RegiLec from './RegiLec.vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const dataList = ref([]);
+const currentPage = ref(0);
+const loginID = ref(0);
+const lec_id = ref(0);
+const modalBoolean = ref(false);
+const lecInfo = ref({});
+const weekplan = ref({});
+
+const modalClose = (param) => {
+  modalBoolean.value = param;
+  LecList();
+};
+
+const modalHandler = (data) => {
+  modalBoolean.value = true;
+  loginID.value = data.loginID;
+  lec_id.value = data.lec_id; // 모달에서 인식할수 있도록 lec_id를 추가적으로 작성 
+
+  let param = new URLSearchParams();
+  param.append('lec_id', lec_id.value);
+
+  axios.post('/std/lecInfo.do', param).then((res) => {
+    lecInfo.value = res.data.lecInfo;
+    weekplan.value = res.data.week_plan;
+    if (res.data.result === 'SUCCESS') {
+      alert(res.data.resultMsg);
+    }
+  }).catch((error) => {
+    console.error('Error fetching lecture info:', error);
   });
-  </script>
-  
-  <style>
-  .cursor-pointer {
-    cursor: pointer;
-  }
-  </style>
-  
+};
+
+const LecList = (cpage = 1) => {
+  let param = new URLSearchParams();
+  param.append('currentPage', cpage);
+  param.append('pageSize', 5);
+  param.append('searchInfo', '');
+  param.append('searchKey', '');
+
+  axios.post('/std/lecListJson.do', param).then((res) => {
+    dataList.value = res.data.lecList;
+    currentPage.value = cpage;
+  }).catch((error) => {
+    console.error('Error fetching lecture list:', error);
+  });
+};
+
+onMounted(() => {
+  LecList();
+});
+</script>
+
+<style>
+.cursor-pointer {
+  cursor: pointer;
+}
+</style>
--- src/views/std/RegiLec.vue
+++ src/views/std/RegiLec.vue
@@ -1,74 +1,102 @@
 <template>
-<div class="backdrop">
-    <div class="container"> 
+    <div class="backdrop">
+      <div class="container">
         <div>
-        <div> 
-            <p>강의 정보</p>
-        </div> 
-            <div> 
-                <div> 
-                    <table>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table>
-                </div>
-                <div> 
-                    <h3>강의 계획</h3>
-                </div>
-                <div> 
-                    <div> 
-                        <table>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
+          <p>강의 정보</p>
+          <div>
+            <table>
+              <tr v-if="lecInfo">
+                <td>강의명</td>
+                <td>{{ lecInfo.lec_name }}</td>
+                <td>수강대상</td>
+                <td>{{ lecInfo.lec_sort }}</td>
+              </tr>
+              <tr v-if="lecInfo">
+                <td>강사명</td>
+                <td>{{ lecInfo.name }}</td>
+                <td>연락처</td>
+                <td>{{ lecInfo.tel }}</td>
+              </tr>
+              <tr v-if="lecInfo">
+                <td>이메일</td>
+                <td>{{ lecInfo.mail }}</td>
+                <td>강의실</td>
+                <td>{{ lecInfo.lecrm_name }}</td>
+              </tr>
+              <tr v-if="lecInfo">
+                <td>수업목표</td>
+                <td colspan="3">{{ lecInfo.learn_goal }}</td>
+              </tr>
+            </table>
+          </div>
+          <div>
+            <h3>강의 계획</h3>
+            <table v-if="lecInfo.weeks">
+              <tr>
+                <td>주차수</td>
+                <td>학습목표</td>
+                <td>학습내용</td>
+              </tr>
+              <tr v-for="(week, index) in lecInfo.weeks" :key="index">
+                <td>{{ week.week }}</td>
+                <td>{{ week.goal }}</td>
+                <td>{{ week.content }}</td>
+              </tr>
+            </table>
+          </div>
         </div>
-        <div> 
-            <button>수강신청</button>
-            <button>닫기</button>
+        <div>
+          <button class="btn btn-light" @click="regi">수강신청</button>
+          <button type="button" class="btn btn-light" @click="$emit('closeModal', false)">닫기</button>
         </div>
+      </div>
     </div>
-</div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  </template>
+  
+  <script setup>
+  import axios from 'axios';
+ 
+  
+  const emit = defineEmits(['closeAndSearch']);
+  const props = defineProps({
+    loginID: Number,
+    lec_id: Number,
+    lecInfo: Object,
+    weekplan: Object,
+  });
+  
+  const regi = () => {
+    let param = new URLSearchParams();
+    param.append('lec_id', props.lec_id);
+    
+    axios.post('/std/lecReg.do', param).then((res) => {
+      if (res.data.result === 'SUCCESS') {
+        alert('수강신청에 성공했습니다');
+        emit('closeAndSearch', false);
+      } else {
+        alert('수강신청에 실패했습니다');
+      }
+    }).catch((error) => {
+      console.error('Error during registration:', error);
+    });
+  };
+  </script>
+  
+  <style>
+  .backdrop {
+    display: flex;
+    position: fixed;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.5);
+  }
+  
+  .container {
+    background: white;
+    padding: 1.5rem;
+    height: 50%;
+  }
+  </style>
+  
